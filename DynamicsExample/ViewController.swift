9bd1bdb2e250451a170c4b4e49f22755ef024d0e
Patrick Doody <w1499693@e-lab-n4112-14.ecs.wmin.ac.uk>
2017-02-22 13:13:52 +0000

    test1

--- DynamicsExample/ViewController.swift
+++ DynamicsExample/ViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
+    
+    private var animator: UIDynamicAnimator!
+    private var gravity: UIGravityBehavior!
+    private var collision: UICollisionBehavior!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let rect = CGRect(x:100, y:100, width:100, height:100)
+        let square = UIView(frame:rect)
+        square.backgroundColor = UIColor.green
+        view.addSubview(square)
+        
+        let barrier = CGRect(x:0, y:300, width:130, height:20)
+        let barriera = UIView(frame:barrier)
+        barriera.backgroundColor = UIColor.red; view.addSubview(barriera)
+        
+        
+        
+        
+        
+        animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior(items: [square]); animator.addBehavior(gravity)
+        
+        collision = UICollisionBehavior(items: [square])
+        
+        collision.collisionDelegate = self
+        
+        func collisionBehavior(_behaviour:UICollisionBehavior, beganContactForItem: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying?, at p: CGPoint){
+            if let unwrapedIdentifier = identifier {
+                print("Boundary contact occurred - \(unwrapedIdentifier)")
+            }
+            else {
+                print("Boundary contact occurred - (unidentified)")
+            }
+        }
+        
+        
+        let rightEdge = CGPoint(x:barriera.frame.origin.x + barriera.frame.size.width,y:barriera.frame.origin.y)
+        collision.addBoundary(withIdentifier:"barriera" as NSCopying, from:barriera.frame.origin, to:rightEdge)
+        
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
     }
 
     override func didReceiveMemoryWarning() {
